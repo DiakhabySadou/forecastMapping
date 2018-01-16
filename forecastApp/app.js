1f6053113d22e71cfc44b2031216701b61324131
LNino <lnino@ensg.eu>
2018-01-16 17:23:23 +0100

    test to write in databas

--- forecastApp/app.js
+++ forecastApp/app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -42,5 +43,45 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+writeToDataBase();
+
+function writeToDataBase()
+{
+  var fs = require('fs');
+  var obj;
+  fs.readFile('./sensors', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+  //  console.log(obj);
+
+    var request = require('request');
+
+    var sensor;
+
+    for (var i in obj.measure)
+    {
+
+
+
+      sensor =obj.measure[i]
+      //console.log(sensor.name);
+      var toWrite = ""+sensor.name+",time="+obj.date;
+          toWrite+=" name="+sensor.desc;
+          toWrite+=" desc="+sensor.unit;
+          toWrite+=" value="+sensor.value;
+          console.log(toWrite);
+      request.post(
+      'http://localhost:8086/write?db=forecast '+toWrite,function (error, response, body)
+      {
+          if (error) {
+              console.log(error)
+          }
+      }
+    //insert temperatures
+);
+
+}
+})
+}
 
 module.exports = app;
