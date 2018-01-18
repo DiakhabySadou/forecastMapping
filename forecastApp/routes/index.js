b6e96eaf293deab9e8a5620fe2cd8ccc00484dc7
ABir <abir.ben-naceur@ensg.eu>
2018-01-18 15:58:11 +0100

    handling interval

--- forecastApp/routes/index.js
+++ forecastApp/routes/index.js
@@ -9,6 +9,18 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.get('/interval', function (req, res)
+{
+  var start = req.query.start;
+  var end = req.query.end;
+  influx.query(`select * from measure where time>${start} and time<${start}`)
+  .then(rows =>
+    {
+      //just comment
+      clearObj(rows);
+      res.send(rows)})
+})
+
 router.get('/last/location', function (req, res)
 {
   influx.query('select * from location GROUP BY * ORDER BY DESC LIMIT 1')
@@ -28,7 +40,7 @@ router.get('/last/rainfall',function(req,res,next)
       res.send(rows);})
 })
 
-router.get('/last/measures',function(req,res,next)
+router.get('/last/measurements',function(req,res,next)
 {
   influx.query('select * from sensors GROUP BY * ORDER BY DESC LIMIT 1')
   .then(rows =>
